5866a04047cf8043344229282a703f2dd2aee1eb
Anton Kabanov <imcreativchik@gmail.com>
2025-03-03 21:35:51 +0500

    initial commit

---
+++ constants.py
@@ -0,0 +1,22 @@
+XPATH_NAME = ("xpath", "//input[@id='name-input']")
+
+INPUT_NAME = "Anton"
+
+XPATH_PASSWORD = ("xpath", "//input[@type='password']")
+
+INPUT_PASSWORD = "2001rice"
+
+XPATH_CHECKBOX = ("xpath", "//input[@type='checkbox']")
+
+XPATH_COLOR = ("xpath", "//input[@id='color3']")
+
+XPATH_DROPDOWN = ("xpath", "//select[@id='automation']")
+
+XPATH_EMAIL = ("xpath", "//input[@id='email']")
+
+INPUT_EMAIL = "test@google.com"
+
+XPATH_TEXTAREA = ("xpath", "//textarea[@id='message']")
+
+XPATH_BUTTON = ("xpath", "//button[@id='submit-btn']")
+
